6a8b73d5c2371b08893d3300182bb3a8836f7839
Daniel O'Sullivan <danielosullivan2007@gmail.com>
2017-09-02 19:43:15 +0100

    added bokeh interactive graphs for aps data

--- Organizer.py
+++ Organizer.py
@@ -13,12 +13,19 @@ import pandas as pd
 import numpy as np
 import datetime as datetime
 import matplotlib.pyplot as plt
+import socket
 
+host = socket.gethostname()
+if host == 'see4-234':
+    source = 'C:\\Users\\eardo\\Desktop\\Barbados Data\\Data Dump\\'
+    Organized ='C:\\Users\\eardo\\Desktop\\Barbados Data\\Barbados_Data\\'
+
+else:
+    source = 'O://Data Dump//'
+    Organized = 'O://Barbados_Data//'
+    
 APSfiles=[]
 files =[]
-source = 'O://Data Dump//'
-Organized = 'O://Barbados_Data//'
-
 uL_start=[]
 uL_end=[]
 uL_start_list=[]
@@ -56,153 +63,167 @@ def INP_calc(x):
     INP = -np.log(1-x)
     return INP
 
-os.chdir(source+'bigN//')
+os.chdir(source+'bigN\\')
 
 bigN_files = [i for i in  glob.glob('*bigN*.csv*')]
 bigN_dates = [i[5:11] for i in bigN_files]
 bigN_dates= list(set(bigN_dates))
 
 #%%Pulls big N files, calculates INPs
+
 for i in range(len(bigN_files)):
-    dt_start = datetime.datetime.strptime(bigN_files[i][5:16],'%y%m%d %H%M')
-    d_start = datetime.datetime.strptime(bigN_files[i][5:11], '%y%m%d')
-    time_end=datetime.datetime.strptime(bigN_files[i][17:21], '%H%M')
-    
-    if 'ON' in bigN_files[i]:
-        dt_end = (datetime.datetime.combine(dt_start.date() + datetime.timedelta(days=1),
-                                           time_end.time()))
-    else:
-        dt_end = (datetime.datetime.combine(dt_start.date(), time_end.time()))
+    try:
+        dt_start = datetime.datetime.strptime(bigN_files[i][5:16],'%y%m%d %H%M')
+        d_start = datetime.datetime.strptime(bigN_files[i][5:11], '%y%m%d')
+        time_end=datetime.datetime.strptime(bigN_files[i][17:21], '%H%M')
         
-    c = dt_end-dt_start
-    t_run = divmod(c.days * 86400 + c.seconds, 60)
-    print ('t_run is {}').format(t_run)
-    runtime=t_run[0]
-    print ('runtime is {}').format(runtime)
-    flow = 16.7
-    washV = bigN_files[i].find('ml')
-    if washV != -1:
-        bigN_files[i][(int(washV)-1)] # only single digit numbers for this in name
-    else:
-        print "\n No wash off volume found in name! \n This code will assume it is 10ml. please change if it is not!\n"
-        washV = 10
-        pass
-    Vdrop = 0.05
-    divisor = Vdrop/washV*flow*runtime
-    big_start.append(dt_start)
-    big_end.append(dt_end)
-    
-   
-    if bigN_files== []:
-        continue
-   
-    else:
         
-        f=i
-        bigNlocs.append(source+'bigN//'+bigN_files[f])
-        #print bigN_files[f]
-        df_INPbig =pd.read_csv(bigN_files[f], delimiter =',',
-                             index_col = False,dtype ={'temp': np.float64}, names=['temp'])
-      #  print df_INPbig.head()
-        count=len(df_INPbig)
-      
+        if 'ON' in bigN_files[i]:
+            dt_end = (datetime.datetime.combine(dt_start.date() + datetime.timedelta(days=1),
+                                               time_end.time()))
+        else:
+            dt_end = (datetime.datetime.combine(dt_start.date(), time_end.time()))
+                
+        
+    
+            
+        c = dt_end-dt_start
+        t_run = divmod(c.days * 86400 + c.seconds, 60)
+       # print ('t_run is {}').format(t_run)
+        runtime=t_run[0]
+        #print ('runtime is {}').format(runtime)
+        flow = 16.7
+        washV = bigN_files[i].find('ml')
+        if washV != -1:
+            bigN_files[i][(int(washV)-1)] # only single digit numbers for this in name
+        else:
+            print "\n No wash off volume found in name! \n This code will assume it is 10ml. please change if it is not!\n"
+            washV = 10
+            pass
+        Vdrop = 0.05
+        divisor = Vdrop/washV*flow*runtime
+        big_start.append(dt_start)
+        big_end.append(dt_end)
+        
        
-        Fraction = df_INPbig.groupby('temp').temp.count().sort_index(ascending = False).cumsum()/len(df_INPbig)
-        #print Fraction.head()
-        df_out = Fraction.apply(INP_calc)/divisor
-        #print df_out.head()
-#%%
-
-  
-
+        if bigN_files== []:
+            continue
+       
+        else:
+            
+            f=i
+            bigNlocs.append(source+'bigN\\'+bigN_files[f])
+            #print bigN_files[f]
+            df_INPbig =pd.read_csv(bigN_files[f], delimiter =',',
+                                 index_col = False,dtype ={'temp': np.float64}, names=['temp'])
+          #  print df_INPbig.head()
+            count=len(df_INPbig)
+          
+           
+            Fraction = df_INPbig.groupby('temp').temp.count().sort_index(ascending = False).cumsum()/len(df_INPbig)
+            #print Fraction.head()
+            df_out = Fraction.apply(INP_calc)/divisor
+            #print df_out.head()
         
+    
+    #%%
+
         fdate = bigN_files[i][5:11]
-        path = (Organized+fdate+'//').strip()
+        path = (Organized+fdate+'\\').strip()
         if not os.path.exists(Organized+fdate):
             os.mkdir(Organized+fdate)
             
-        if not os.path.exists(Organized+fdate+'//bigN//'):
-            os.mkdir(Organized+fdate+'//bigN//')
+        if not os.path.exists(Organized+fdate+'\\bigN\\'):
+            os.mkdir(Organized+fdate+'\\bigN\\')
        # print i,bigN_files[i]
-        df_out.to_csv(Organized+fdate+'//'+bigN_files[i].strip('.txt'))
-        print bigN_files[i]
-        print df_out.head()
+        df_out.to_csv(Organized+fdate+'\\'+bigN_files[i].strip('.txt'))
+        #print bigN_files[i]
+        #print df_out.head()
         #print Organized+fdate+'//'+bigN_files[i].strip('.txt')
-os.chdir (source + 'uL NIPI//')
-uL_folders =[i for i in glob.glob('*//')]
+    
+    except ValueError:
+        print 'incorrect format: {}'.format(bigN_files[i])
+        continue
 
-#==============================================================================
-# for j in range(len(uL_folders)):
-#     os.chdir(source + 'uL NIPI//' + uL_folders[j])
-#     analysis_list = [k for k in glob.glob('*Data*.csv')]
-#     date_list = [m[5:11]+'//' for m in analysis_list]
-#     file_info = [l for l in zip(analysis_list, date_list)]
-#     
-#     
-#     for z in range (len(file_info)):
-#         if not os.path.exists(Organized+date_list[z]):
-#             os.mkdir(Organized+date_list[z])
-#         print z
-#         shutil.copy ((os.getcwd()+'\\'+file_info[z][0]),Organized+date_list[z])
-#         
-#==============================================================================
+os.chdir (source + 'uL NIPI\\')
+uL_folders =[i for i in glob.glob('*\\')]
+
+for j in range(len(uL_folders)):
+    os.chdir(source + 'uL NIPI//' + uL_folders[j])
+    analysis_list = [k for k in glob.glob('*Data*.csv')]
+    date_list = [m[5:11]+'//' for m in analysis_list]
+    file_info = [l for l in zip(analysis_list, date_list)]
+    
+    
+    for z in range (len(file_info)):
+        if not os.path.exists(Organized+date_list[z]):
+            os.mkdir(Organized+date_list[z])
+        print z
+        shutil.copy ((os.getcwd()+'\\'+file_info[z][0]),Organized+date_list[z])
+        
         
 #%% Scans for uL *data files
 
-
 for i in range(len(bigN_dates)):
-    os.chdir(Organized+'//'+ bigN_dates[i])
-    c=glob.glob('Data*.csv')##
-    missing_uNIPI=[]
-        
-    if c==[]:
-        no_uL=1
-    else:
-        no_uL=0
-    if not c:
-        no_data_flag=1
-       # print 'uL NIPI *Data.CSV files missing {}'.format(bigN_dates[i])
-       # missing_NIPI.append(analysis_day)
-    else:    
-        no_data_flag=0
-        
-        for j in range(len(c)):
-           
-            if 'Blan' in c[j]:
-                
-                continue
-
-            else:
-                
-                uL_start= c[j][5:11]+"_"+c[j][17:21]
-                uL_end = c[j][5:11]+"_"+c[j][22:26]
-             
-                uL_start = (datetime.datetime.strptime(uL_start, '%y%m%d_%H%M'))
-                uL_start_list.append(uL_start)
-                
-                uL_end_datetime =  datetime.datetime.strptime(uL_end, '%y%m%d_%H%M')
-
-    
-                if 'ON' in c[j]:
-
-                    x= (uL_end_datetime + datetime.timedelta(days=1))
-                    uL_end_list.append(x)
-                    print 
-                    uL.append(Organized+ bigN_dates[i]+'//'+c[j])
+    try: 
+        os.chdir(Organized+'\\'+ bigN_dates[i])
+        print bigN_dates[i]
+        c=glob.glob('Data*.csv')##
+        missing_uNIPI=[]
+            
+        if c==[]:
+            no_uL=1
+        else:
+            no_uL=0
+        if not c:
+            no_data_flag=1
+           # print 'uL NIPI *Data.CSV files missing {}'.format(bigN_dates[i])
+           # missing_NIPI.append(analysis_day)
+        else:    
+            no_data_flag=0
+            
+            for j in range(len(c)):
+                print c[j]
+                uL_start =[]
+                uL_end = []
+                if 'Blan' in c[j]:
                     
-                elif '2N' in c[j]:
-                    x= (uL_end_datetime + datetime.timedelta(days=2))
-                    uL_end_list.append(x)
-                    uL.append(Organized+ bigN_dates[i]+'//'+c[j])
-       
+                    continue
+    
                 else:
-                   
-                   x=datetime.datetime.strptime(uL_end, '%y%m%d_%H%M')
-                   uL_end_list.append(x)
-                   uL.append(Organized+ bigN_dates[i]+'//'+c[j])
-                   print bigN_dates,c[j]
-                   
-                   
+                    
+                    uL_start= c[j][5:11]+"_"+c[j][17:21]
+                    uL_end = c[j][5:11]+"_"+c[j][22:26]
+                 
+                    uL_start = (datetime.datetime.strptime(uL_start, '%y%m%d_%H%M'))
+                    uL_start_list.append(uL_start)
+                    #print uL_start_list
+                    print '~~~~~~~~~~~~~~~~'
+                    uL_end_datetime =  datetime.datetime.strptime(uL_end, '%y%m%d_%H%M')
+                    
+        
+                    if 'ON' in c[j]:
+    
+                        x= (uL_end_datetime + datetime.timedelta(days=1))
+                        uL_end_list.append(x)
+            
+                        uL.append(Organized+ bigN_dates[i]+'//'+c[j])
+                        
+                    elif '2N' in c[j]:
+                        x= (uL_end_datetime + datetime.timedelta(days=2))
+                        uL_end_list.append(x)
+                        uL.append(Organized+ bigN_dates[i]+'//'+c[j])
+           
+                    else:
+                       
+                       x=datetime.datetime.strptime(uL_end, '%y%m%d_%H%M')
+                       uL_end_list.append(x)
+                       uL.append(Organized+ bigN_dates[i]+'//'+c[j])
+                       #print bigN_dates,c[j]
+    except WindowsError:
+        print 'directory missing'
+        continue               
+                       
 big_N = pd.DataFrame([[i,j] for i,j in zip(big_start, big_end)])
 uL_dt = pd.DataFrame([[i,j] for [i,j] in zip(uL_start_list, uL_end_list)])
 bigNlocs=pd.DataFrame(bigNlocs)
@@ -226,13 +247,16 @@ for i in range(len(uL_dt)):
             t_run = divmod(c.days * 86400 + c.seconds, 60)
             runtime=t_run[0]
             flow = 16.7
-            washV = bigN_files[i].find('ml')
-            if washV != -1:
-                bigN_files[i][(int(washV)-1)] # only single digit numbers for this in name
-            else:
-                print "\n No wash off volume found in name! \n This code will assume it is 10ml. please change if it is not!\n"
-                washV = 10
-                pass
+            washV = 10
+            print washV
+#==============================================================================
+#             if washV != -1:
+#                 bigN_files[i][(int(washV)-1)] # only single digit numbers for this in name
+#             else:
+#                 print "\n No wash off volume found in name! \n This code will assume it is 10ml. please change if it is not!\n"
+#                 washV = 10
+#                 pass
+#==============================================================================
             Vdrop = 0.05
             divisor = Vdrop/washV*flow*runtime
             bigdata = pd.read_csv(big.iloc[0].tolist()[0], names=['temp'])
@@ -247,62 +271,61 @@ for i in range(len(uL_dt)):
             ax2 =plt.scatter (uL_data.iloc[:,0], uL_data.iloc[:,1],label = uL[i])
             ax1.set_yscale('log')
             ax1.set_ylim(0.0001, 30)
+            plt.savefig('compare'+ str(i))
             plt.legend()
             
             
-#==============================================================================
-# #%% Scans all folders in organized and plots
-# 
-# topfolder='O:\\Barbados_Data\\'
-# 
-# bigN_list =[]
-# bigN_data = pd.DataFrame(columns = {'T', 'INP'})
-# 
-# 
-# uL_list =[]
-# uL_data = pd.DataFrame(columns = {'T', 'INP'})
-# 
-# 
-# os.chdir(topfolder)
-# folders = glob.glob('*\\')
-# loc = []
-# for i in range(len(folders)):
-#     os.chdir(topfolder+ folders[i])
-#     z=folders[i]
-#     if glob.glob('*bigN*.csv') ==[]:
-#         continue
-#     else:
-#         
-#         x=[topfolder+ z+ j for j in glob.glob('*bigN*.csv')]
-#         bigN_list.extend(x)
-#         
-# for i in range(len(folders)):
-#     os.chdir(topfolder+ folders[i])
-#     z=folders[i]       
-#         
-#     if glob.glob('*Data*.csv') ==[]:
-#         continue
-#     else:
-#         
-#         y=[topfolder+ z+ j for j in glob.glob('*Data*.csv')]
-#         uL_list.extend(y)
-# 
-# for i in range (len(bigN_list)):
-#     y=pd.read_csv(bigN_list[i], names=['T', 'INP'])
-#     bigN_data = bigN_data.append(y, ignore_index = True)
-# for i in range (len(uL_list)):
-#     y=pd.read_csv(uL_list[i], names=['T', 'INP'], usecols = [0,1], skiprows=1)
-#     uL_data = uL_data.append(y, ignore_index = True)
-#      
-# fig =plt.plot()
-# plt.scatter(bigN_data['T'], bigN_data['INP'])
-# 
-# plt.scatter(uL_data['T'], uL_data['INP'])
-# plt.yscale('log')
-# plt.ylim(0.0001,30)            
-#             
-#             
-#==============================================================================
+#%% Scans all folders in organized and plots
+
+topfolder=Organized
+
+bigN_list =[]
+bigN_data = pd.DataFrame(columns = {'T', 'INP'})
+
+
+uL_list =[]
+uL_data = pd.DataFrame(columns = {'T', 'INP'})
+
+
+os.chdir(topfolder)
+folders = glob.glob('*\\')
+loc = []
+for i in range(len(folders)):
+    os.chdir(topfolder+ folders[i])
+    z=folders[i]
+    if glob.glob('*bigN*.csv') ==[]:
+        continue
+    else:
+        
+        x=[topfolder+ z+ j for j in glob.glob('*bigN*.csv')]
+        bigN_list.extend(x)
+        
+for i in range(len(folders)):
+    os.chdir(topfolder+ folders[i])
+    z=folders[i]       
+        
+    if glob.glob('*Data*.csv') ==[]:
+        continue
+    else:
+        
+        y=[topfolder+ z+ j for j in glob.glob('*Data*.csv')]
+        uL_list.extend(y)
+
+for i in range (len(bigN_list)):
+    y=pd.read_csv(bigN_list[i], names=['T', 'INP'])
+    bigN_data = bigN_data.append(y, ignore_index = True)
+for i in range (len(uL_list)):
+    y=pd.read_csv(uL_list[i], names=['T', 'INP'], usecols = [0,1], skiprows=1)
+    uL_data = uL_data.append(y, ignore_index = True)
+     
+fig2 =plt.plot()
+plt.scatter(bigN_data['T'], bigN_data['INP'])
+
+plt.scatter(uL_data['T'], uL_data['INP'])
+plt.yscale('log')
+plt.ylim(0.0001,30)            
+            
+            
 
 
 
--- aps puller.py
+++ aps puller.py
@@ -11,23 +11,46 @@ import glob as glob
 import matplotlib.dates as mdates
 import datetime
 import matplotlib.pyplot as plt
+import socket
+import pickle
+import bokeh
+from bokeh import mpl
+from bokeh.plotting import figure
+#from bokeh.io import output_file, show
+from bokeh.charts import TimeSeries, output_file, show
 #import Organizer
 
-Organized = 'O:\\Barbados_Data\\'
-os.chdir(Organized)
-x = glob.glob(Organized + '*\\')
-df_APS= pd.DataFrame()
-aps_list=[]
-for i in range (len(x)):
-    os.chdir(x[i]+'APS\\')
-    aps_files = glob.glob('*.txt')
-    for f in range(len(aps_files)):
-        aps_list.append(aps_files[f])
-        df_APSreader=pd.read_csv(aps_files[f], delimiter =',', header =6,  usecols = range(4, 56)) 
-        df_APSreader['datetime']=pd.to_datetime (pd.read_csv(aps_files[f], delimiter =',', header =6).iloc[:, 1]+" "+ 
-                    pd.read_csv(aps_files[f], delimiter =',', header =6).iloc[:, 2])
-        df_APS=df_APS.append(df_APSreader)    
+if socket.gethostname() == 'see4-234':
+    Organized = 'C:\\Users\\eardo\\Desktop\\Barbados Data\\Barbados_Data\\'
+    picdir =  'C:\\Users\\eardo\\Desktop\\Barbados Data\\Pickles\\'
+else:
+    Organized = 'O:\\Barbados_Data\\'
+
+##THE FOLLOWING SECTION SEARCHES THE FILES, AND THEN PICKLES THE DATA 
+#==============================================================================
+
+# os.chdir(Organized)
+# x = glob.glob(Organized + '*\\')
+# df_APS= pd.DataFrame()
+# aps_list=[]
+# 
+# for i in range (len(x)):
+#     try:
+#         os.chdir(x[i]+'APS\\')
+#         aps_files = glob.glob('*.txt')
+#         for f in range(len(aps_files)):
+#             aps_list.append(aps_files[f])
+#             df_APSreader=pd.read_csv(aps_files[f], delimiter =',', header =6,  usecols = range(4, 56)) 
+#             df_APSreader['datetime']=pd.to_datetime (pd.read_csv(aps_files[f], delimiter =',', header =6).iloc[:, 1]+" "+ 
+#                         pd.read_csv(aps_files[f], delimiter =',', header =6).iloc[:, 2])
+#             df_APS=df_APS.append(df_APSreader)
+#     except WindowsError:
+#         continue
+# df_APS.to_pickle(picdir+('APS'))
+# 
+#==============================================================================
        #%% 
+df_APS = pd.read_pickle(picdir+'APS')   
 df_APS['Total'] = df_APS.iloc[:,0:52].sum(axis =1)
 df_APS['datetime']=df_APS['datetime']-datetime.timedelta(hours =5)
 df_APS.sort_values(by=['datetime'], inplace = True)
@@ -39,8 +62,21 @@ line1, = ax1.plot(df_APS.index, df_APS['Total'])
 ax1.set_ylabel('APS particles cm$^{-3}$')
 ax1.set_xlabel('Date')
 
-resample = df_APS.resample('1H', how = 'SUm)
+resample = df_APS.resample('1H').mean().reset_index()
+resample2 = df_APS.resample('1D').mean().reset_index()
 resample.to_csv(Organized + 'all_APS.csv')
+
+order = ['Date','Conc']
+unaveraged = pd.DataFrame(dict( Conc = df_APS['Total'],Date=df_APS.index)).reindex(columns=order)
+one_hr = pd.DataFrame({ 'Date' : resample.datetime,'Conc' : resample.Total}).reindex(columns=order)
+daily = pd.DataFrame({ 'Date' : resample2.datetime,'Conc' : resample2.Total}).reindex(columns=order)
+#p = TimeSeries(xyvalues, title = 'APS Timeseries', color = 'blue', xlabel = 'Date',
+p1 = figure(x_axis_type="datetime",x_axis_label = 'Date', y_axis_label = 'APS concentration \cc', title="APS Data")
+p1.line(x= unaveraged.Date, y=unaveraged.Conc, legend ='Raw Data')
+p1.line(x=one_hr.Date, y = one_hr.Conc, color = 'red', line_width =3, legend = 'Hourly mean')
+p1.line(x=daily.Date, y = daily.Conc, color = 'black', line_width =3, legend ='Daily mean' )
+output_file(Organized+'APS_total.html')
+show(p1)
 
 #==============================================================================
 # #%%df_APS['Total'] = df_APS.iloc[:,0:52].sum(axis =1)
