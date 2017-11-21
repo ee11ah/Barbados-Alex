5ae002609e34c954921257c119d8b37eab70f129
Alexander Harrison [ee11ah] <ee11ah@leeds.ac.uk>
2017-11-21 16:58:22 +0000

    latest copy

--- INP code barbados (test).py
+++ INP code barbados (test).py
@@ -15,19 +15,29 @@ import numpy as np
 import os
 
 BigNfiles = []
-#read in aps and met data
+#read in big nipi data 
 path = 'C:\Users\ee11ah\Desktop\Barbados_Data\\'
 for root, dirs, files in os.walk(path):
     for file in files:
         if file.startswith('big'):
             a =os.path.relpath(root) +'\\'
-            b = file
-            a = a+b
+            bb = file
+            a = a+bb
             BigNfiles.append(a)
             df = pd.read_csv(a, names = ["Temps", "?"])
             #df['Temps']= df(c0)
-            df['NuT/Na']= 0
+            b = len(df.Temps)
+            c= df.index +1
+            df['event_number']= c
+            df['FFS']=df.event_number/b
+            df['NuT/Na']= (b-df.event_number)/b
+            df['INP per Liter'] = 0
+            #df.to_csv(file, delimiter=',')
+            
+            print c
+            print a
             print df.head()
+            
 
             
     
