e0a2b461bfcea43ef021c0c9288324050dd6d67c
Alexander Harrison [ee11ah] <ee11ah@leeds.ac.uk>
2017-11-23 16:08:58 +0000

    new seperate codes

---
+++ Creates CSVs for data.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 23 15:58:37 2017
+
+@author: ee11ah
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 21 12:03:06 2017
+
+@author: ee11ah
+"""
+
+import pandas as pd
+from pandas import DataFrame
+import datetime
+import csv
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+BigNfiles = []
+#read in big nipi data 
+path = 'C:\Users\ee11ah\Desktop\Barbados_Data\\'
+for root, dirs, files in os.walk(path):
+    for file in files:
+        #looks for big nipi files
+        if file.startswith('big'):
+            a =os.path.relpath(root) +'\\'
+            bb = file
+            a = a+bb
+            #appends file name to a list for later use
+            BigNfiles.append(a)
+            #adds the column titles
+            df = pd.read_csv(a, names = ["Temps", "?"])
+            #gets len of the column for calculation
+            b = len(df.Temps)
+            #uses index number to get event numbers
+            c= df.index +1
+            #creates new coloumns for calculations
+            df['event_number']= c
+            df['FFS']=df.event_number/b
+            df['NuT']= (b-df.event_number)/b
+            #df['INP per Liter'] = 0
+            #delets unneccesary coloumns
+            del df['event_number']
+            del df ['?']
+            #takes the time from the file names and converts them to a time difference time in minutes
+            df['time sampled'] =((int(bb[17:19])-int(bb[12:14]))*60)-(((int(bb[19:21])))-((int(bb[14:16]))))
+            stm=((int(bb[17:19])-int(bb[12:14]))*60)-(((int(bb[19:21])))-((int(bb[14:16]))))
+            df['time sampled'] = stm
+            #calculates the volume of air sampled
+            df['volume_air']=16.7*stm
+            # adds further time on if the run was done over night
+            ON=bb.find("ON")
+            twoN=bb.find("2N")
+            ml=bb.find("6m")
+            if ON != -1:
+                stm=((24*60)-((int(bb[12:14])*60)+int(bb[14:16]))) 
+                mte=((int(bb[17:19])*60)+int(bb[19:21]))
+                df['time sampled']=stm+mte
+                df['volume_air']=16.7*df['time sampled']
+            if twoN != -1:
+                stm=((24*60)-((int(bb[12:14])*60)+int(bb[14:16]))) 
+                mte=((int(bb[17:19])*60)+int(bb[19:21]))
+                df['time sampled']=stm+mte+(24*60)
+                df['volume_air']=16.7*['time sampled']
+            if ml != -1:
+                 df['INP']=-np.log((df.NuT))*(float(6)/(0.05*df.volume_air))
+                 df['volUsed']=6
+            else:
+                df['INP']=-np.log((df.NuT))*(float(10)/(0.05*df.volume_air))
+                df['volUsed']=10
+            df.to_csv('NEW' + bb)           
---
+++ Graph Plotter.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 23 16:07:22 2017
+
+@author: ee11ah
+"""
+import pandas as pd
+from pandas import DataFrame
+import datetime
+import csv
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+folder = 'C:\Users\ee11ah\Desktop\Barbados_Data_Test\\'
+for root, dirs, files in os.walk(folder):
+    for file in files:
+        for file in files:
+        #Created files
+            plt.figure(1)
+            if file.startswith('NEW'):
+                a =os.path.relpath(root) +'\\'
+                bb = file
+                a = a+bb
+                df = pd.read_csv(a)
+                plt.scatter(df.Temps, df.INP)
+                plt.title('INP per liter Barbados')
+                plt.ylabel('INP per liter')
+                plt.yscale('log')
+                plt.xlabel('Temperature degrees celsius')
+plt.show(1)
